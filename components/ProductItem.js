135123dc7e87f2ab51aaf7fdfcc566e6b00a984e
farzanatabassum <farzanatabassum531@gmail.com>
2022-10-24 23:59:59 +0600

    silly issues

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -1,14 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
 
 const ProductItem = ({image,brand,category,size,price}) => {
   return (
     <div>
    <Link href='/product/Regalia'>     
 <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-        <img className="rounded-t-lg object-contain object-center h-[56vh] w-full" src={image} alt=""/>
-   
+        <img className="rounded-t-lg object-contain object-center h-[56vh] w-full" src={image} alt="cloth image"/>
     <div className="p-5">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{brand}</h5>
         <h5 className="mb-2 font-normal text-gray-700 dark:text-gray-400">
