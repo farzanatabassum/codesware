02089e77aebb1ef9a4aeec177d5b22214d878024
farzanatabassum <farzanatabassum531@gmail.com>
2022-10-24 23:47:22 +0600

    image added

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -1,26 +1,25 @@
 /* eslint-disable @next/next/no-img-element */
+import Link from 'next/link'
 import React from 'react'
 
 const ProductItem = ({image,brand,category,size,price}) => {
   return (
     <div>
-        
-<div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
-        <img class="rounded-t-lg object-contain object-center h-[56vh] w-full" src={image} alt=""/>
-    </a>
-    <div class="p-5">
-        <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{brand}</h5>
-        </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            <h5>{category}</h5>
-            <h5>{size}</h5>
-            <h5>{price}</h5>
-        </p>
+   <Link href='/product/Regalia'>     
+<div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <img className="rounded-t-lg object-contain object-center h-[56vh] w-full" src={image} alt=""/>
+   
+    <div className="p-5">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{brand}</h5>
+        <h5 className="mb-2 font-normal text-gray-700 dark:text-gray-400">
+            {category}      
+        </h5>
+        <h5 className="mb-2 font-normal text-gray-700 dark:text-gray-400">{size}</h5>
+        <h5 className="mb-2 font-normal text-gray-700 dark:text-gray-400">{price}</h5>
         
     </div>
 </div>
+</Link>
 
     </div>
 
--- pages/cloth.js
+++ pages/cloth.js
@@ -6,16 +6,17 @@ const cloth = () => {
     <div>
       
 
-      <section class="py-10 px-12">
-    <div class="grid grid-flow-row gap-8 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-            <ProductItem image='/dress1.jpg' brand='Yellow' category='Dress' size='Size: M' price='2000'/>
-            <ProductItem image='/men casual.jpg' brand='Yellow' category='Dress'size='Size: M' price='2000'/>
-            <ProductItem image='/lehenga2.jpg' brand='Yellow' category='Dress' size='Size: M' price='2000'/>
-            <ProductItem image='/shirt.jpg' brand='Yellow' category='Dress'size='Size: M' price='2000'/>
-            <ProductItem image='/gown1.jpg' brand='Yellow' category='Dress' size='Size: M' price='2000'/>
-            <ProductItem image='/suit1.jpg' brand='Yellow' category='Dress' size='Size: M' price='2000'/>
-            <ProductItem image='/tshirt.jpg' brand='Yellow' category='Dress'size='Size: M' price='2000'/>
-            <ProductItem image='/dress2.jpg' brand='Yellow' category='Dress' size='Size: M' price='2000'/>
+      <section className="py-10 px-12">
+    <div className="grid grid-flow-row gap-8 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+            <ProductItem image='/dress1.jpg' brand='Yellow' category='Dress' size='Size: M' price='Tk 2000'/>
+            <ProductItem image='/lehenga2.jpg' brand='Anzara' category='Lehenga' size='Size: L' price='Tk 5000'/>
+            <ProductItem image='/shirt.jpg' brand='Vogue' category='Shirt'size='Size: L' price='Tk 2500'/>
+            <ProductItem image='/gown1.jpg' brand='Tahoor' category='Abaya' size='Size: L' price='Tk 3000'/>
+            <ProductItem image='/suit1.jpg' brand='Yellow' category='Suit' size='Size: M' price='TK 6000'/>
+            <ProductItem image='/dress2.jpg' brand='Aarong' category='Dress' size='Size: S' price='2000'/>
+            <ProductItem image='/men casual.jpg' brand='Ecstasy' category='Casual'size='Size: XL' price='Tk 3000'/>
+            <ProductItem image='/tshirt.jpg' brand='Vogue' category='Tshirt'size='Size: L' price='Tk 3000'/>
+
        
     </div>
 </section>
